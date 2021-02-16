6b5f6295d305cffa10fe156ec3d4bc39cb92c03e
jca26c <78522594+jca26c@users.noreply.github.com>
2021-02-15 18:03:39 -0600

    Update script.js

--- script.js
+++ script.js
@@ -14,6 +14,27 @@ require([
           center: [-98.6, 39.6] // longitude, latitude
         });
   
+const fire_symbol_renderer = {
+  type: "heatmap",
+  colorStops: [
+    { color: "rgba(63, 40, 102, 0)", ratio: 0 },
+    { color: "#472b77", ratio: 0.083 },
+    { color: "#4e2d87", ratio: 0.166 },
+    { color: "#563098", ratio: 0.249 },
+    { color: "#5d32a8", ratio: 0.332 },
+    { color: "#6735be", ratio: 0.415 },
+    { color: "#7139d4", ratio: 0.498 },
+    { color: "#7b3ce9", ratio: 0.581 },
+    { color: "#853fff", ratio: 0.664 },
+    { color: "#a46fbf", ratio: 0.747 },
+    { color: "#c29f80", ratio: 0.83 },
+    { color: "#e0cf40", ratio: 0.913 },
+    { color: "#ffff00", ratio: 1 }
+  ],
+  maxPixelIntensity: 200,
+  minPixelIntensity: 0
+};
+  
           
         /********************
          * Add feature layer
@@ -24,9 +45,9 @@ require([
    
    
    var featureLayer_fire_activity = new FeatureLayer({
-     url:"https://services9.arcgis.com/RHVPKKiFTONKtxq3/arcgis/rest/services/Satellite_VIIRS_Thermal_Hotspots_and_Fire_Activity/FeatureServer"
+     url:"https://services9.arcgis.com/RHVPKKiFTONKtxq3/arcgis/rest/services/Satellite_VIIRS_Thermal_Hotspots_and_Fire_Activity/FeatureServer",
+     renderer: fire_symbol_renderer
    });
 
-        map.add(featureLayer_wildfires);
-  map.add(featureLayer_fire_activity);
+         map.add(featureLayer_fire_activity); map.add(featureLayer_wildfires);
       });
